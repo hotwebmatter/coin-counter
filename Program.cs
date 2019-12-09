716618b3a8e52046fc848d277fc9d804ab5aec3a
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-09 16:21:15 -0500

    Implement change-making algorithm
    Thanks, CS50 :)

--- Program.cs
+++ Program.cs
@@ -7,7 +7,28 @@ namespace coin_counter
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            const int CHANGE = 92;
+            int changeRemaining,
+                quarters,
+                dimes,
+                nickels,
+                pennies;
+            // implement classic change-making algorithm
+
+            changeRemaining = CHANGE;
+            quarters = changeRemaining / 25;
+            changeRemaining = changeRemaining % 25;
+            dimes = changeRemaining / 10;
+            changeRemaining = changeRemaining % 10;
+            nickels = changeRemaining / 5;
+            changeRemaining = changeRemaining % 5;
+            pennies = changeRemaining;
+
+            WriteLine("Original change amount: {0:C2}", CHANGE);
+            WriteLine("Amount of quarters: {0}", quarters);
+            WriteLine("Amount of dimes: {0}", dimes);
+            WriteLine("Amount of nickels: {0}", nickels);
+            WriteLine("Amount of pennies: {0}", pennies);
         }
     }
 }

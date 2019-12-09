c78eabbd2f66dde4cb1c1de6686f2a237b224738
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-09 16:06:14 -0500

    Initial commit of coin-counter solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coin_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
